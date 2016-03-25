17462eff95886069c39f3a14bc62c300884261b6
Arinerron <Arinerron@users.noreply.github.com>
2016-03-24 22:48:14 -0700

    Syntax Error CI Test #1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception { // This is just a test, so this shouldn't matter that I am throwing Exception
         System.out.println("This needs a main class I think, so here is a simple one.");
-        
+        This is a syntax error test for the ci {okay?0)];213
+        ;4'5'678l;67754rt';ugfsdjo;isd
+        vc
+    2899@#$%^&*()*(645trgfbvoijlky5ersfcx)ghdfhdghdfghfdfghg    f
+    
+    [                                          ^That should error if the CI is working            ] < not centered xD
+    
+    
         /* 
          * I was going to make a simple program with the ScratchAPI here, but I don't have an alt account
          * that I don't care if the password is shown, and Scratch won't let me register any accounts
