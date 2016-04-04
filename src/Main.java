b34ecfbb5c3f1c8c45278a23be51848a6da898b6
Arinerron <arinesau@gmail.com>
2016-04-03 22:40:35 -0700

    Changed Main.java message

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws Exception { // This is just a test, so this shouldn't matter that I am throwing Exception
-        System.out.println("This needs a main class I think, so here is a simple one.");
-        
-        /* 
-         * I was going to make a simple program with the ScratchAPI here, but I don't have an alt account
-         * that I don't care if the password is shown, and Scratch won't let me register any accounts
-         * (the register page doesn't load)
-         *
-         */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Delete this main class, it is just for TravisCI.");
     }
 }
